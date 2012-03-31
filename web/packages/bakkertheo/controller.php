1e8d61e03fa33412f66780326b7e5531fb6a5393
Willem <willem@unime.nl>
2012-03-31 20:20:38 +0200

    now really working

---
+++ web/packages/bakkertheo/controller.php
@@ -0,0 +1,29 @@
+<?php    
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+class BakkertheoPackage extends Package {
+
+	protected $pkgHandle = 'bakkertheo';
+	protected $appVersionRequired = '5.5.0';
+	protected $pkgVersion = '1.0';
+	
+	public function getPackageDescription() {
+		return t("Installs the Bakker theo theme");
+	}
+	
+	public function getPackageName() {
+		return t("Bakkerij Theo Hoek");
+	}
+	
+	public function install() {
+		$pkg = parent::install(); 
+		
+		// install the theme		
+		PageTheme::add('bakkertheo', $pkg);		
+	}
+
+
+
+
+}
